462c1d98bc3ddd99990279ae2390d1f293ac94ca
Marco Benevides <marcoabf@gmail.com>
2020-06-01 13:27:15 -0300

    notifications

--- public/js/vue.js
+++ public/js/vue.js
@@ -1,7 +1,7 @@
 //import { text } from "body-parser";
 //axios.defaults.baseURL = 'http://localhost:3000';
-axios.defaults.baseURL = 'http://192.168.1.109:3000';
-//axios.defaults.baseURL = 'https://digiacesso.net/';
+//axios.defaults.baseURL = 'http://192.168.1.109:3000';
+axios.defaults.baseURL = 'https://digiacesso.net/';
 //axios.defaults.baseURL = 'http://digiacesso-net.umbler.net/';
 axios.defaults.withCredentials = true;
 
